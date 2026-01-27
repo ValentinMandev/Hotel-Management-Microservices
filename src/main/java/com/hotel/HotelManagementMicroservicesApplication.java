eb341d2166c762d580ec81f499669dab86e9b6b0
ValentinMandev <valentin.vkm@gmail.com>
2026-01-27 22:22:27 +0200

    Initial commit: Hotel Management Microservices project structure

---
+++ src/main/java/com/hotel/HotelManagementMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementMicroservicesApplication.class, args);
+    }
+
+}
