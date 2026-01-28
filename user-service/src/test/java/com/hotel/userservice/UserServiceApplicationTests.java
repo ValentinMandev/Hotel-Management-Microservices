7106f6716d8806b57b323d5d063bcf0a61ee0b39
ValentinMandev <valentin.vkm@gmail.com>
2026-01-28 21:41:18 +0200

    Adding user service structure

--- user-service/src/test/java/com/hotel/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/hotel/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hotel;
+package com.hotel.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HotelManagementMicroservicesApplicationTests {
+class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
