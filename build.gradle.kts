71fdc9d65dc086e557a1a6fc0169f8d19a4e369b
ValentinMandev <valentin.vkm@gmail.com>
2026-01-29 22:35:51 +0200

    Adding application configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<Test> {
