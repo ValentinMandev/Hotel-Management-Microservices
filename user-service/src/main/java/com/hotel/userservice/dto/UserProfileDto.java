681be0017db0cc0410a56d097b6920d63e607101
ValentinMandev <valentin.vkm@gmail.com>
2026-02-14 20:57:06 +0200

    Add User profile dto

--- user-service/src/main/java/com/hotel/userservice/dto/UserProfileDto.java
+++ user-service/src/main/java/com/hotel/userservice/dto/UserProfileDto.java
@@ -1,9 +1,23 @@
 package com.hotel.userservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserProfileDto {
+
+    @NotEmpty(message = "Please enter your email address")
+    private String email;
+
+    @NotEmpty(message = "Please enter your first name")
+    private String firstName;
+
+    @NotEmpty(message = "Please enter your last name")
+    private String lastName;
+
+    @NotEmpty(message = "Please enter your phone number")
+    private String phoneNumber;
+
 }
