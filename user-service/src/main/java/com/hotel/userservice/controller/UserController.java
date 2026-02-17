6125ff7be0027277f8eee49bd9cdb70c7790c6e0
ValentinMandev <valentin.vkm@gmail.com>
2026-02-17 19:06:26 +0200

    Add User controller

--- user-service/src/main/java/com/hotel/userservice/controller/UserController.java
+++ user-service/src/main/java/com/hotel/userservice/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.hotel.userservice.controller;
 
+import com.hotel.userservice.dto.UserProfileDto;
+import com.hotel.userservice.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/profile")
+    public String updateProfile(@Valid @RequestBody UserProfileDto request) {
+        return null;
+    }
 }
