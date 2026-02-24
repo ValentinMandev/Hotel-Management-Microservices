0c5934c60a88c9d1c7c4c828e933d09f1334e2f8
ValentinMandev <valentin.vkm@gmail.com>
2026-02-24 20:42:22 +0200

    Add booking entity

---
+++ booking-service/src/main/java/com/hotel/bookingservice/model/Booking.java
@@ -0,0 +1,23 @@
+package com.hotel.bookingservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String bookingReference;
+
+
+}
