6573e580913fdda0d5e6a8166c08b392321a332e
ValentinMandev <valentin.vkm@gmail.com>
2026-02-27 16:10:05 +0200

    Create booking controller

---
+++ booking-service/src/main/java/com/hotel/bookingservice/controller/BookingController.java
@@ -0,0 +1,13 @@
+package com.hotel.bookingservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/booking")
+public class BookingController {
+
+    @Autowired
+    BookingService bookingService;
+}
